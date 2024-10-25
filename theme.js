8f2eb31cc23d42fc9656ee9996f280d11e352aa3
William <bill.demitz@gmail.com>
2024-10-25 03:02:45 +0200

    colors

---
+++ theme.js
@@ -0,0 +1,24 @@
+// Set color palette overrides
+t.prefs_.set('color-palette-overrides', [
+  "#080808", // black
+  "#D70000", // red
+  "#789978", // green
+  "#ffAA88", // yellow
+  "#7788AA", // blue
+  "#4f2e4f", // magenta
+  "#708090", // cyan
+  "#deeeed", // white
+  "#555555", // bright black (dark gray)
+  "#D70000", // bright red
+  "#789978", // bright green
+  "#ffAA88", // bright yellow
+  "#7788AA", // bright blue
+  "#4f2e4f", // bright magenta
+  "#708090", // bright cyan
+  "#ffffff"  // bright white (light gray)
+]);
+
+// Set foreground, background, and cursor colors
+t.prefs_.set('foreground-color', "#deeeed"); // white
+t.prefs_.set('background-color', "#080808"); // black
+t.prefs_.set('cursor-color', 'rgba(222, 238, 237, 0.5)'); // semi-transparent white
