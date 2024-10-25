a299293a9f04059005c481f238c7cf9981b0da4c
William <bill.demitz@gmail.com>
2024-10-25 03:20:55 +0200

    blacks

--- theme.js
+++ theme.js
@@ -1,6 +1,6 @@
 // Set color palette overrides
 t.prefs_.set('color-palette-overrides', [
-  "#080808", // black
+  "#000000", // black
   "#D70000", // red
   "#789978", // green
   "#ffAA88", // yellow
@@ -8,7 +8,7 @@ t.prefs_.set('color-palette-overrides', [
   "#4f2e4f", // magenta
   "#708090", // cyan
   "#deeeed", // white
-  "#555555", // bright black (dark gray)
+  "#080808", // bright black (dark gray)
   "#D70000", // bright red
   "#789978", // bright green
   "#ffAA88", // bright yellow
@@ -20,5 +20,5 @@ t.prefs_.set('color-palette-overrides', [
 
 // Set foreground, background, and cursor colors
 t.prefs_.set('foreground-color', "#deeeed"); // white
-t.prefs_.set('background-color', "#080808"); // black
+t.prefs_.set('background-color', "#191919"); // black
 t.prefs_.set('cursor-color', 'rgba(222, 238, 237, 0.5)'); // semi-transparent white
